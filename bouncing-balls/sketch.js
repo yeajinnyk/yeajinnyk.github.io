d4ad68d5401159273f06e3ad2f00dd73d8f00206
yeajinnyk <77638065+yeajinnyk@users.noreply.github.com>
2021-01-19 13:37:26 -0800

    bouncing ball demo
    arrays, objects, for loops

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -1,9 +1,12 @@
-// Project Title
-// Your Name
-// Date
+// Bouncing Balls (Array Demo)
+// Jinny Kim
+// 01. 19. 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+let theBouncingBalls = [];
+
 
 
 function setup() {
@@ -11,7 +14,44 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("black");
+  moveBall();
+  displayBall();
 }
 
-//beep boop
+function mousePressed() {
+  let ball = {
+    x: mouseX,
+    y: mouseY,
+    diameter: random(25, 100),
+    dx: random(-5, 5),
+    dy: (random -5, 5),
+    theColor: color(random(255), random(255), random(255), random(255)), //4th value is opacity
+  };
+  theBouncingBalls.push(ball);
+}
+
+function moveBall() {
+  for (let ball of theBouncingBalls) { //*** 
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  
+    //check for bounce
+    if (ball.x + ball.diameter/2 >= width ||
+      ball.x - ball.diameter/2 <= 0) {
+      ball.dx *= -1;
+    }
+    if (ball.y + ball.diameter/2 >= height ||
+      ball.y - ball.diameter/2 <= 0) {
+      ball.dy *= -1;
+    }
+  }
+}
+
+function displayBall() {
+  for (let i = 0; i < theBouncingBalls.length; i++) { //***for loops can interchange. they do the same thing
+    noStroke();
+    fill(theBouncingBalls[i].theColor);
+    ellipse(theBouncingBalls[i].x, theBouncingBalls[i].y, theBouncingBalls[i].diameter, theBouncingBalls[i].diameter);
+  }
+}
