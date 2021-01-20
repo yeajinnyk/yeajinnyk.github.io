22fe1c5a1dec6958a26eea74fd73e401edb202e9
yeajinnyk <77638065+yeajinnyk@users.noreply.github.com>
2021-01-20 13:48:50 -0600

    perlin noisedemo

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -1,17 +1,45 @@
-// Project Title
-// Your Name
-// Date
+// Perlin Noise
+// Jinny Kim
+// 01. 20. 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rectHeights;
+let bikeLocation = 0;
+let generateHowMany = 10000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectHeights = generateHeights();
 }
 
 function draw() {
   background(220);
-  ellipse(100, 200, 50, 50);
+
+  let howMany = width;
+  
+  for (let i = bikeLocation; i < bikeLocation + howMany; i++) {
+    let rectWidth = width / howMany;
+    rect(rectWidth * (i - bikeLocation), height - rectHeights[i], rectWidth, rectHeights[i]);
+  }
+
+  if (keyIsPressed) {
+    if (key === "d") {
+      bikeLocation += 5;
+    }
+    if (key === "a" && bikeLocation > 0) {
+      bikeLocation -= 5;
+    }
+  }
+  
 }
 
+function generateHeights() {
+  let theHeights = [];
+  for (let i = 0; i < generateHowMany; i++) {
+    let rectHeight = noise(i * 0.001) * height; 
+    theHeights.push(rectHeight);
+  }
+  return theHeights;
+}
