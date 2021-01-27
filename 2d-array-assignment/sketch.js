2ae01e53abe73b51c78005234cd121c027636e42
yeajinnyk <77638065+yeajinnyk@users.noreply.github.com>
2021-01-27 14:09:59 -0600

    getting started

--- 2d-array-assignment/sketch.js
+++ 2d-array-assignment/sketch.js
@@ -1,15 +1,58 @@
-// Project Title
-// Your Name
-// Date
+// 2D Array Assignment
+// Jinny Kim
+// 01. 27. 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let grid = createEmptyBoard();
+let rows, cols, cellSize;
+
+
+//https://editor.p5js.org/2402183/sketches/-9p5VEm4e
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else if (windowHeight > windowWidth) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowWidth, windowHeight);
+  }
+
+  rows = grid.length;
+  cols = grid[0].length;
 }
 
 function draw() {
   background(220);
+  displayBoard();
+}
+
+function createEmptyBoard() {
+  let emptyBoard = [];
+
+  for (let y = 0; y < 3; y++) {
+    emptyBoard.push([]);
+    for (let x = 0; x < 3; x++) {
+      emptyBoard[y].push(0);
+    }
+  }
+  return emptyBoard;
+}
+
+function displayBoard() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        console.log("O");
+      }
+      if (grid[y][x] === 1) {
+        console.log("X");
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
 }
