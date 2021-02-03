f2e9ac2381667595397d9c26587133669ee92953
yeajinnyk <77638065+yeajinnyk@users.noreply.github.com>
2021-02-03 13:01:41 -0800

    oop ball demo

---
+++ ball-bounce/sketch.js
@@ -0,0 +1,81 @@
+// OOP Ball Bounce Demo
+// Jinny Kim
+// 02. 03. 2021
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("black");
+
+  for (let i = 0; i < ballArray.length; i++) {
+    for (let j = 0; j < ballArray.length; j++) {
+      //collision check
+      if (i !== j) {
+        //don't check self for collision
+        ballArray[i].checkIfCollidingWith(ballArray[j]);
+      }
+    }
+    ballArray[i].move();
+    ballArray[i].display();
+  }
+}
+
+function mousePressed() {
+  let theBall = new Ball(mouseX, mouseY, random(10, 40));
+  ballArray.push(theBall);
+}
+
+class Ball {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
+    this.someColor = color(random(255), random(255), random(255), random(255));
+  }
+
+  display() {
+    noStroke();
+    fill(this.someColor);
+    ellipse(this.x, this.y, this.radius*2, this.radius*2);
+  }
+
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    //bounce on walls
+    if (this.x - this.radius < 0 || this.x + this.radius > width) {
+      this.dx *= -1;
+    }
+    if (this.y - this.radius < 0 || this.y + this.radius > height) {
+      this.dy *= -1;
+    }
+  }
+
+  checkIfCollidingWith(otherBall) {
+    let sumOfRadii = this.radius + otherBall.radius;
+    let distanceBetweenCenters = dist(this.x, this.y, otherBall.x, otherBall.y);
+
+    if (sumOfRadii > distanceBetweenCenters) {
+      //temporary thing
+      // this.someColor = "red";
+      // otherBall.someColor = "red";
+
+      //react to collision
+      let tempDx = this.dx;
+      let tempDy = this.dy;
+
+      this.dx = otherBall.dx;
+      otherBall.dx = tempDx;
+
+      this.dy = otherBall.dy;
+      otherBall.dy = tempDy;
+    }
+  }
+}
