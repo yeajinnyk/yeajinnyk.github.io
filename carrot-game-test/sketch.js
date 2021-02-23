2e0cb5f2a0b79a223ddad583dc5349e6cbbecb4f
yeajinnyk <77638065+yeajinnyk@users.noreply.github.com>
2021-02-23 00:40:55 -0800

    Incorporated catching the carrots, scoring system, and health system

--- carrot-game-test/sketch.js
+++ carrot-game-test/sketch.js
@@ -3,12 +3,9 @@
 // 02. 17. 2021
 
 //NEXT TO WORK ON: 
-// - catching carrots
-// - fix interval of spawning carrots
-// - incorporate health system (hearts)
-// - scoring?
-
-//Amy's game: https://editor.p5js.org/3802012/sketches/7BIZxrCEL
+// - fix interval of spawning carrots (ask mr. schellenberg)
+// - incorporate death screen
+// - incorporation back button
 
 let carrotImg, carrotGameBg, basketImg;
 let basketWidth, basketHeight;
@@ -18,31 +15,46 @@ let carrotSize = 40;
 
 let carrots = [];
 
+let fullHealthImg, twoHeartsImg, oneHeartImg, deadImg;
+let healthBarWidth, healthBarHeight, healthBarX, healthBarY;
+
+let health, score, points;
+let carrotGamePlaying = true;
+
 class Carrot {
   constructor() {
     this.x = random(width);
     this.y = 0;
     this.dx = 0;
     this.dy = random(5, 10);
-    this.notCaught = true;
     this.size = carrotSize;
-    
+  }
+
+  notCaught() {
+    if (this.x > basketX && this.x < basketX + basketWidth && this.y + this.size > basketY && this.y < basketY + basketHeight) {
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
   move() {
-    if (this.y + carrotSize < height) {
+    if (this.y + this.size < height && this.notCaught()) {
       this.y += this.dy;
-    }
-    else if (this.notCaught) {
-      this.notCaught = false;
     }
   }
 
   display() {
-    if (this.notCaught) {
+    if (this.notCaught() && this.onScreen()) {
       image(carrotImg, this.x, this.y, this.size, this.size);
     }
   }
+
+  onScreen() {
+    return this.y + this.size < height;
+  }
+  
 }
 
 function spawnCarrot() {
@@ -52,8 +64,13 @@ function spawnCarrot() {
 
 function dropCarrot() {
   for (let i = carrots.length - 1; i >= 0; i--) {
-    if (!carrots[i].notCaught) {
+    if (!carrots[i].notCaught()) { //caught!
       carrots.splice(i, 1);
+      points++;
+    }
+    else if (!carrots[i].onScreen()) { //missed carrot
+      carrots.splice(i, 1);
+      health--;
     }
     else {
       carrots[i].move();
@@ -66,8 +83,12 @@ function preload() {
   carrotImg = loadImage("assets/good-carrot.png");
   carrotGameBg = loadImage("assets/carrot-game-bg.png");
   basketImg = loadImage("assets/basket.png");
-}
 
+  fullHealthImg = loadImage("assets/threeHearts.png");
+  twoHeartsImg = loadImage("assets/twoHearts.png");
+  oneHeartImg = loadImage("assets/oneHeart.png");
+  deadImg = loadImage("assets/noHearts.png");
+}
 
 function setup() {
   if (windowWidth > windowHeight) {
@@ -86,7 +107,46 @@ function setup() {
   basketX = width/2;
   basketY = height * 0.65;
 
+  healthBarWidth = fullHealthImg.width * 0.5;
+  healthBarHeight = fullHealthImg.height * 0.5;
+  healthBarX = width * 0.7;
+  healthBarY = height * 0.9;
+
+  points = 0;
+  health = 3;
+
   window.setInterval(spawnCarrot, 1500); //use something else
+}
+
+function carrotScore() {
+  score = "Score: " + points;
+  fill("black");
+  textSize(20);
+  textFont("VERDANA");
+  textAlign(CENTER);
+  text(score, width/2, height * 0.96);
+}
+
+function displayHealthBar() {
+  if (health === 3) {
+    image(fullHealthImg, healthBarX, healthBarY, healthBarWidth, healthBarHeight);
+  }
+  else if (health === 2) {
+    image(twoHeartsImg, healthBarX, healthBarY, healthBarWidth, healthBarHeight);
+  }
+  else if (health === 1) {
+    image(oneHeartImg, healthBarX, healthBarY, healthBarWidth, healthBarHeight);
+  }
+  else {
+    image(deadImg, healthBarX, healthBarY, healthBarWidth, healthBarHeight);
+    carrotGamePlaying = false;
+  }
+}
+
+function displayLossScreen() {
+  if (!carrotGamePlaying) {
+    background("pink"); //temp
+  }
 }
 
 function displayCarrotGameBg() {
@@ -123,4 +183,8 @@ function draw() {
   keepBasketIn();
 
   dropCarrot();
+
+  displayHealthBar();
+  carrotScore();
+  displayLossScreen();
 }
